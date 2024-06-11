aa593596e1c907e1e3010f91f68f97cd955c0a2c
jamesc66 <james@ghomelink.co>
2024-06-11 23:43:19 +0100

    dynamic tables

--- src/pages/TablePage.vue
+++ src/pages/TablePage.vue
@@ -2,17 +2,10 @@
   <div>
     <p v-if="loading">Loading...</p>
     <div v-else>
-      <h1>Users Table</h1>
-      <TableComponent :data="data.users" :columns="columns.users" />
-
-      <h1>Brands Table</h1>
-      <TableComponent :data="data.brands" :columns="columns.brands" />
-
-      <h1>Products Table</h1>
-      <TableComponent :data="data.products" :columns="columns.products" />
-
-      <h1>Reviews Table</h1>
-      <TableComponent :data="data.reviews" :columns="columns.reviews" />
+      <div v-for="table in columns" :key="table.id">
+        <h1>{{ formatTableName(table.id) }}</h1>
+        <TableComponent :data="data[table.id]" :columns="table.data" />
+      </div>
     </div>
   </div>
 </template>
@@ -29,11 +22,22 @@ export default {
   },
   setup() {
     const { data, columns, loading } = useDataStore();
-    console.log(data);
+
+    const formatTableName = (tableName) => {
+      return (
+        tableName.charAt(0).toUpperCase() +
+        tableName
+          .slice(1)
+          .replace(/([A-Z])/g, " $1")
+          .trim()
+      );
+    };
+
     return {
       data: computed(() => data.value),
       columns: computed(() => columns.value),
       loading: computed(() => loading.value),
+      formatTableName,
     };
   },
 };
